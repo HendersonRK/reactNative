baecb431f7fe5209c7c6e9e041b2301248ba6a03
HendersonRK <henderson.kettermann@gmail.com>
2024-11-14 20:04:39 -0300

    aplicativo feito em aula

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Contador } from './src/components/Contador';
+import { Account } from './src/components/Account';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Account/>
       <StatusBar style="auto" />
     </View>
   );
